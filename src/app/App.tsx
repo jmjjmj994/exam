3e10336e30def23770740c0d8ec57be4134387fc
jmjjmj <jonathan.mjos@outlook.com>
2024-05-25 12:56:14 +0200

    helmet in app comp

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,12 @@
-import { Helmet } from "react-helmet-async";
-
+import { Helmet } from 'react-helmet-async';
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      app
+    </div>
+  );
 };
