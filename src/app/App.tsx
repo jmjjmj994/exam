98159bc501bff9ed1a2a0ccfa46a052e8f8e169f
jmjjmj <jonathan.mjos@outlook.com>
2024-05-25 15:48:15 +0200

    rendering cards

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,42 @@
 import { Helmet } from 'react-helmet-async';
-import { Venues } from './venues/Venues';
+import { useFetchVenues } from 'src/api/fetch/use-fetch-venues.hook';
+import { VenuesCard } from './VenuesCard';
+import { VenuesSkeleton } from './VenuesSkeleton';
+import styles from './styles.module.css';
 export const App = () => {
+  const { data, isLoading, error } = useFetchVenues(1);
+  const filterData = data.data.filter(
+    (venue) =>
+      venue.location.address &&
+      venue.location.city &&
+      venue.location.country &&
+      venue.name.length < 30
+  );
   return (
     <div>
       <Helmet>
         <title>Home</title>
       </Helmet>
-      <Venues />
+      <div className={styles.app_grid}>
+        {isLoading &&
+          Array.from({ length: 30 }).map((_, index) => (
+            <VenuesSkeleton key={index} />
+          ))}
+        {filterData.map(
+          ({ id, name, media, location, rating, price, owner }) => (
+            <VenuesCard
+              key={id}
+              id={id}
+              name={name}
+              media={media}
+              location={location}
+              rating={rating}
+              price={price}
+              owner={owner}
+            />
+          )
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/app/VenuesCard.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { checkUrlValid } from 'src/api/utilities/utilities';
+import { Star } from 'phosphor-react';
+import { VenueType } from 'src/api/validation/venue-schema';
+
+const renderImage = (imageUrl: string, name: string) => {
+  return (
+    <img
+      loading="lazy"
+      className="h-full w-full  aspect-square object-cover rounded-md"
+      src={imageUrl}
+      alt={`image of ${name}`}
+    />
+  );
+};
+
+export const VenuesCard: React.FC<VenueType> = ({
+  id,
+  name,
+  media,
+  location,
+  rating,
+  price,
+  owner,
+}) => {
+  const [isValidUrl, setIsValidUrl] = useState(true);
+  useEffect(() => {
+    const checkUrlValidity = async () => {
+      if (!media || media.length === 0) return;
+      const imageUrl = media[0].url;
+      try {
+        const isValid = await checkUrlValid(imageUrl);
+        setIsValidUrl(isValid);
+      } catch (error) {
+        console.error('Error fetching URL:', error);
+        setIsValidUrl(false);
+      }
+    };
+    checkUrlValidity();
+  }, [media]);
+
+  return (
+    <article className="flex flex-col relative rounded-md">
+      <Link className="absolute w-full h-full" to={''}></Link>
+      {isValidUrl
+        ? media && media.length > 0
+          ? renderImage(media[0].url, name)
+          : renderImage(
+              '/public/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.avif',
+              `Placeholder for ${name}`
+            )
+        : renderImage(
+            '/public/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.avif',
+            `Placeholder for ${name}`
+          )}
+
+      <div className="min-h-[10vh]">
+        <div className="flex justify-between items-center">
+          {' '}
+          <p className="font-int-bold">
+            {location?.city ? location.city : 'No city provided'}, {''}
+            {location?.country ? location.country : 'No location provided'}
+          </p>
+          <div className="flex items-center  gap-1">
+            <Star />
+            <p>{rating}</p>
+          </div>
+        </div>
+        <div>
+          <p className="font-int-light">Host: {owner?.name}</p>
+          <p className="font-int-bold"> ${price} night</p>
+        </div>
+      </div>
+    </article>
+  );
+};
