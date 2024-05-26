e1d92dc40efa640bce923c4a47a3aff932b5ad5d
jmjjmj <jonathan.mjos@outlook.com>
2024-05-26 21:38:51 +0200

    finish

--- src/routes/checkout/Checkout.tsx
+++ src/routes/checkout/Checkout.tsx
@@ -4,15 +4,40 @@ import { CheckoutPayment } from './CheckoutPayment';
 import { CheckoutSuccess } from './CheckoutSuccess';
 import { useFormStep } from 'src/state/useStore';
 
-const CheckoutStepIndication = () => {
+const CheckoutStepIndicator = () => {
   const { formStep } = useFormStep();
-
   switch (formStep) {
     case 0:
-      return <p>hei 0</p>;
+      return (
+        <div className="flex w-full max-w-[50rem] items-center justify-between">
+          <span
+            className={`bg-custom-primary text-white flex items-center justify-center w-[2.5rem] h-[2.5rem] border border-custom-primary rounded-full`}
+          >
+            1
+          </span>
+          <span
+            className={`flex items-center justify-center w-[2.5rem] h-[2.5rem] border border-custom-primary rounded-full`}
+          >
+            2
+          </span>
+        </div>
+      );
     case 1:
-      return <p>hei1</p>;
-    case 2:
+      return (
+        <div className="flex w-full max-w-[50rem] items-center justify-between">
+          <span
+            className={`flex items-center justify-center w-[2.5rem] h-[2.5rem] border border-custom-primary rounded-full`}
+          >
+            1
+          </span>
+          <span
+            className={`bg-custom-primary text-white flex items-center justify-center w-[2.5rem] h-[2.5rem] border border-custom-primary rounded-full`}
+          >
+            2
+          </span>
+        </div>
+      );
+    default:
       return null;
   }
 };
@@ -25,29 +50,13 @@ export const Checkout = () => {
       <Helmet>
         <title>Holidaze | checkout</title>
       </Helmet>
-      {formStep === 2 ? '' : <h1 className="text-left">Checkout</h1>}
+      <h1 className="text-left bg-orange-500">Checkout</h1>
       <div className=" w-full flex-col flex justify-center items-center gap-4">
+        <CheckoutStepIndicator />
+        {formStep === 0 && <CheckoutForm />}
+        {formStep === 1 && <CheckoutPayment />}
         {formStep === 2 && <CheckoutSuccess />}
-        <div className="flex w-full max-w-[50rem]  items-center justify-between">
-          <span
-            className={`${
-              formStep === 0 && 'bg-custom-primary text-white'
-            } flex items-center justify-center w-[2.5rem] h-[2.5rem] border border-custom-primary rounded-full`}
-          >
-            1
-          </span>
-          <hr className="w-[85%]" />
-          <span
-            className={`${
-              formStep === 1 && 'bg-custom-primary text-white'
-            } flex items-center justify-center w-[2.5rem] h-[2.5rem] border border-custom-primary  rounded-full`}
-          >
-            2
-          </span>
-        </div>
-        {formStep === 0 ? <CheckoutForm /> : <CheckoutPayment />}
       </div>
-      {/*    <CheckoutForm /> */}
     </section>
   );
 };
