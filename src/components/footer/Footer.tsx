bfdf34ca1a1ad71f2359f5fcbffc6c095faf7c05
jmjjmj <jonathan.mjos@outlook.com>
2024-05-28 21:00:11 +0200

    added footer content

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,22 @@
-
+import { footerLinks } from './footer-links';
 export const Footer = () => (
   <footer className="footer footer-l bg-custom-background_white">
-    <div className="element-wrapper">Wrapper</div>
+    <div className="element-wrapper">
+      <div className="footer_wrapper w-full flex flex-wrap ">
+        {footerLinks.map((section, index) => (
+          <div key={index} className=" sm:w-1/2 md:w-1/3 lg:w-1/5 p-4 py-10">
+            <h3 className="">{section.title}</h3>
+            <ul>
+              {section.items.map((item, index) => (
+                <li className="text-custom-textWeak text-sm" key={index}>
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
   </footer>
 );
 
-/* <div className="footer_wrapper w-full flex flex-wrap ">
-      {footerLinks.map((section, index) => (
-        <div key={index} className=" sm:w-1/2 md:w-1/3 lg:w-1/5 p-4 py-10">
-          <h3 className="">{section.title}</h3>
-          <ul>
-            {section.items.map((item, index) => (
-              <li className="text-custom-textWeak text-sm" key={index}>
-                {item}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div> */
