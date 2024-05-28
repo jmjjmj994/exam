9c6209577b5eab2fb6933aad9922d9bca936bd69
jmjjmj <jonathan.mjos@outlook.com>
2024-05-28 23:35:17 +0200

    removing unused imports

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -1,5 +1,5 @@
 import { SearchForm } from './SearchForm';
-import { Faders } from 'phosphor-react';
+
 export const SearchBar = () => {
   return (
     <div className="h-[2.5rem] flex gap-4 max-w-[40rem] w-full m-auto">
